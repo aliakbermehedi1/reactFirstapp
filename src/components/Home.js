b90abc1b8e2cb1ad643e100ac4b11259d1cd5301
Ali Akber Mehedi <78915374+aliakbermehedi1@users.noreply.github.com>
2024-05-18 21:17:29 +0600

    Done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,54 +1,9 @@
 import React from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import * as Yup from "yup";
-import { InputText } from "primereact/inputtext";
-import { Button } from "primereact/button"; // Import Button component from PrimeReact
 
 const Home = () => {
-  const initialValues = {
-    name: "",
-    email: "",
-  };
-
-  const validationSchema = Yup.object({
-    name: Yup.string().required("Name is required"),
-    email: Yup.string()
-      .email("Invalid email address")
-      .required("Email is required"),
-  });
-
-  const handleSubmit = (values, { setSubmitting }) => {
-    console.log(values);
-    setSubmitting(false);
-  };
-
   return (
-    <div>
-      <h2>Booking Form</h2>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={handleSubmit}
-      >
-        <Form>
-          <div>
-            <label htmlFor="name">Name</label>
-            <Field name="name">
-              {({ field }) => <InputText id="name" {...field} />}
-            </Field>
-            <ErrorMessage name="name" component="div" />
-          </div>
-          <div>
-            <label htmlFor="email">Email</label>
-            <Field name="email">
-              {({ field }) => <InputText id="email" {...field} />}
-            </Field>
-            <ErrorMessage name="email" component="div" />
-          </div>
-          {/* Use PrimeReact Button for form submission */}
-          <Button type="submit" label="Submit" className="p-button-danger" />
-        </Form>
-      </Formik>
+    <div className="text-center font-bold text-6xl h-screen mt-56">
+     <div className="animate-pulse">SPOT BOOKING</div>
     </div>
   );
 };
